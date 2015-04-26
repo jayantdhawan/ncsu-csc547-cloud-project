673721e29dc9311033324a2c34a38c212d070a11
gargsajal9 <gargsajal9@gmail.com>
2015-04-25 23:06:35 -0400

    Pushing the mapper module.

--- mapper.py
+++ mapper.py
@@ -5,180 +5,231 @@ import psycopg2.extras
 import time
 import datetime
 
-def user_storage_grp_insert (user_id, storage_grp_id):
-	
-	try:
-        	conn = psycopg2.connect("dbname='test' user='transuser' host='172.12.24.10' password='transcirrus1'")
+class Mapper:
 
-	except:
-        	print "Error!!! Cannot connect"
+	@staticmethod
+	def insert_user_storage_grp (user_id, storage_grp_id):
+	
+		try:
+        		conn = psycopg2.connect("dbname='test' user='transuser' host='172.12.24.10' password='transcirrus1'")
 
-	
-	try:
-	
-		cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-		#query = "INSERT INTO user_cinder(user_id, cinder_id) VALUES ("
-	
-		ts = time.time()
-	#	print ts
-		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-	#	print st
-	
-		cur.execute("Insert INTO user_storage_grp(user_id, storage_grp_id, date_timestamp) VALUES(%s,%s, %s)",(user_id,storage_grp_id,st))
-		conn.commit()
+		except:
+        		print "Error!!! Cannot connect"
 
-	except psycopg2.DatabaseError, e:
+		try:
+	
+			cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+			#query = "INSERT INTO user_cinder(user_id, cinder_id) VALUES ("
+	
+			ts = time.time()
+		#	print ts
+			st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+		#	print st
+	
+			cur.execute("Insert INTO user_storage_grp(user_id, storage_grp_id, date_timestamp) VALUES(%s,%s, %s)",(user_id,storage_grp_id,st))
+			conn.commit()
+
+		except psycopg2.DatabaseError, e:
     
-    		if conn:
-        		conn.rollback()
+			if conn:
+        			conn.rollback()
     
-    		print 'Error %s' % e
+    			print 'Error %s' % e
 
-	finally:
+		finally:
     
-    		if conn:
-        		conn.close()
+    			if conn:
+        			conn.close()
 	
-	return;
+		return;
 
-def storage_grp_cinder_insert (storage_grp_id, cinder_id):
+	@staticmethod
+	def insert_storage_grp_cinder (storage_grp_id, cinder_id):
 
-        try:
-                conn = psycopg2.connect("dbname='test' user='transuser' host='172.12.24.10' password='transcirrus1'")
+        	try:
+                	conn = psycopg2.connect("dbname='test' user='transuser' host='172.12.24.10' password='transcirrus1'")
 
-        except:
-                print "Error!!! Cannot connect"
+        	except:
+                	print "Error!!! Cannot connect"
+
+        	try:
+
+                	cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+                	#query = "INSERT INTO user_cinder(user_id, cinder_id) VALUES ("
+
+			ts = time.time()
+	         #       print ts
+        	        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	         #       print st
+
+        	        cur.execute("Insert INTO storage_grp_cinder(storage_grp_id,cinder_id, date_timestamp) VALUES(%s,%s,%s)",(storage_grp_id,cinder_id,st))
+                	conn.commit()
+
+	        except psycopg2.DatabaseError, e:
+
+        	        if conn:
+                	        conn.rollback()
+
+	                print 'Error %s' % e
+
+        	finally:
+
+                	if conn:
+                        	conn.close()
+
+	        return;
+
+	@staticmethod
+	def get_user_from_cinder (cinder_id):
+
+		myList=[]
+	        
+		try:
+        	        conn = psycopg2.connect("dbname='test' user='transuser' host='172.12.24.10' password='transcirrus1'")
+
+	        except:
+        	        print "Error!!! Cannot connect"
 
 
-        try:
+		try:
 
-                cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-                #query = "INSERT INTO user_cinder(user_id, cinder_id) VALUES ("
+			cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+                	#query = "INSERT INTO user_cinder(user_id, cinder_id) VALUES ("
 
-                ts = time.time()
-         #       print ts
-                st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-         #       print st
+			cur.execute("Select distinct us.user_id from user_storage_grp us, storage_grp_cinder sg where us.storage_grp_id = sg.storage_grp_id and sg.cinder_id = %s",(cinder_id))
 
-                cur.execute("Insert INTO storage_grp_cinder(storage_grp_id,cinder_id, date_timestamp) VALUES(%s,%s,%s)",(storage_grp_id,cinder_id,st))
-                conn.commit()
-
-        except psycopg2.DatabaseError, e:
-
-                if conn:
-                        conn.rollback()
-
-                print 'Error %s' % e
-
-        finally:
-
-                if conn:
-                        conn.close()
-
-        return;
-
-def user_cinder_fetch_user (cinder_id):
-
-	myList=[]
-        try:
-                conn = psycopg2.connect("dbname='test' user='transuser' host='172.12.24.10' password='transcirrus1'")
-
-        except:
-                print "Error!!! Cannot connect"
-
-
-	try:
-
-		cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-                #query = "INSERT INTO user_cinder(user_id, cinder_id) VALUES ("
-
-		cur.execute("Select distinct us.user_id from user_storage_grp us, storage_grp_cinder sg where us.storage_grp_id = sg.storage_grp_id and sg.cinder_id = %s",(cinder_id))
-
-		rows = cur.fetchall()
-		i=0;
-		for row in rows:
-			myList.append(row[0])
-                	i=i+1
+			rows = cur.fetchall()
+			i=0;
 	
-	except psycopg2.DatabaseError, e:
+			for row in rows:
+				myList.append(row[0])
+                		i=i+1
+	
+		except psycopg2.DatabaseError, e:
 
-		if conn:
-			conn.rollback()
+			if conn:
+				conn.rollback()
 
-		print 'Error %s' % e
+			print 'Error %s' % e
 
-	finally:
+		finally:
 
-		if conn:
-			conn.close()
+			if conn:
+				conn.close()
         
-	return myList;
+		return myList;
+
+	@staticmethod
+	def get_cinder_from_user (user_id):
+
+		myList=[]
+	
+	        try:
+        	        conn = psycopg2.connect("dbname='test' user='transuser' host='172.12.24.10' password='transcirrus1'")
+
+	        except:
+        	        print "Error!!! Cannot connect"
+
+	        try:
+
+        	        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+                	#query = "INSERT INTO user_cinder(user_id, cinder_id) VALUES ("
+
+        	        cur.execute("Select distinct sg.cinder_id from storage_grp_cinder sg, user_storage_grp us where us.storage_grp_id = sg.storage_grp_id and us.user_id = %s",(user_id))
+	
+                	rows = cur.fetchall()
+
+	                for row in rows:
+        	                myList.append(row[0])
+
+	        except psycopg2.DatabaseError, e:
 
 
-def user_cinder_fetch_cinder (user_id):
+        	        if conn:
+                	        conn.rollback()
 
-	myList=[]
-        try:
-                conn = psycopg2.connect("dbname='test' user='transuser' host='172.12.24.10' password='transcirrus1'")
+	                print 'Error %s' % e
 
-        except:
-                print "Error!!! Cannot connect"
+        	finally:
 
-
-        try:
-
-                cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-                #query = "INSERT INTO user_cinder(user_id, cinder_id) VALUES ("
-
-                cur.execute("Select distinct sg.cinder_id from storage_grp_cinder sg, user_storage_grp us where us.storage_grp_id = sg.storage_grp_id and us.user_id = %s",(user_id))
-
-                rows = cur.fetchall()
-
-                for row in rows:
-                        myList.append(row[0])
-
-        except psycopg2.DatabaseError, e:
+                	if conn:
+                        	conn.close()
 
 
-                if conn:
-                        conn.rollback()
+	        return myList;
 
-                print 'Error %s' % e
+	@staticmethod
+	def check_exist_user_cinder (user_id, cinder_id):
+ 	
+		try:
+                	conn = psycopg2.connect("dbname='test' user='transuser' host='172.12.24.10' password='transcirrus1'")
 
-        finally:
+	        except:
+        	        print "Error!!! Cannot connect"
 
-                if conn:
-                        conn.close()
+	        try:
+
+        	        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+                	#query = "INSERT INTO user_cinder(user_id, cinder_id) VALUES ("
+
+	                cur.execute("Select count(*) as cnt from storage_grp_cinder sg, user_storage_grp us where us.storage_grp_id = sg.storage_grp_id and us.user_id = %s and sg.cinder_id = %s",(user_id,cinder_id))
+
+                	rows = cur.fetchall()
+
+	                output = rows[0][0]
+	#		print("op - ",output)
+
+        	except psycopg2.DatabaseError, e:
 
 
-        return myList;
+                	if conn:
+                        	conn.rollback()
+
+	                print 'Error %s' % e
+
+        	finally:
+
+                	if conn:
+                        	conn.close()
+
+	        if output == 0L:
+	#		print("Inside 0L");
+			return bool(False);
+		else:
+	#		print("Inside 1L");
+			return bool(True);
+
+
 
 print "Hello World"
 
-#print user_storage_grp_insert('a',1)
-#print user_storage_grp_insert('b',1)
-#print user_storage_grp_insert('a',2)
-#print storage_grp_cinder_insert(1,'c')
-#print storage_grp_cinder_insert(1,'d')
-#print storage_grp_cinder_insert(2,'e')
-#print storage_grp_cinder_insert(2,'f')
+mapper = Mapper()
+#print mapper.insert_user_storage_grp('a',1)
+#print mapper.insert_user_storage_grp('b',1)
+#print mapper.insert_user_storage_grp('a',2)
+#print mapper.insert_storage_grp_cinder(1,'c')
+#print mapper.insert_storage_grp_cinder(1,'d')
+#print mapper.insert_storage_grp_cinder(2,'e')
+#print mapper.insert_storage_grp_cinder(2,'f')
 
-arr = user_cinder_fetch_user('e')
+arr = mapper.get_user_from_cinder('e')
 
 for element in arr:
 	print "User-",element
 
-cinder = user_cinder_fetch_cinder('b')
+cinder = mapper.get_cinder_from_user('b')
 
 for element in cinder:
 	print "Cinder - ",element
 
-ts = time.time()
-#print ts
-st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-#print st
-#rows = cur.fetchall()
 
-#print "\nShow me the databases:\n"
-#for row in rows:
- #   print "   ", row[0]
+if(mapper.check_exist_user_cinder('a','c')):
+	print("c-a Cinder-User mapping found");
+else:
+	print("c-a Cinder-User mapping not found");
+
+if(mapper.check_exist_user_cinder('b','f')):
+        print("b-f Cinder-User mapping found");
+else:
+        print("b-f Cinder-User mapping not found");
+
